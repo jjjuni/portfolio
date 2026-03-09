718feda992f27f7363aad4d5273c8e1bf17e1880
jjjuni <0210leejun@gmail.com>
2026-03-09 17:48:30 +0900

    ✨ feat: 전체 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,67 @@
-import Header from "./component/Header";
+import { useEffect } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Header from "./component/header/Header";
+import About from "./component/about/About";
+import Projects from "./component/projects/Projects";
+import Skills from "./component/skills/Skills";
+import useLenisScroll from "./hooks/useLenisScroll";
+import Closing from "./component/closing/Closing";
+import useModalStore from "./stores/useModalStore";
 
 function App() {
 
+  const { isModalOpen } = useModalStore();
+
+  const { stop, start, lenisRef } = useLenisScroll();
+
+  useEffect(() => {
+    if ("scrollRestoration" in history) {
+      history.scrollRestoration = "manual";
+    }
+    ScrollTrigger.clearScrollMemory();
+
+    requestAnimationFrame(() => {
+      window.scrollTo(0, 0);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isModalOpen) {
+      stop();
+      document.body.style.overflow = "hidden";
+    }
+    else {
+      start();
+      document.body.style.overflow = "";
+    }
+  }, [isModalOpen])
+
+  useEffect(() => {
+    const originalOverflow = document.body.style.overflow;
+    document.body.style.overflow = "hidden";
+
+    const timer = setTimeout(() => {
+      document.body.style.overflow = originalOverflow || "auto";
+    }, 1500);
+
+    return () => {
+      clearTimeout(timer);
+      document.body.style.overflow = originalOverflow;
+    };
+  }, []);
+
   return (
-    <div className={`bg-bg w-screen h-screen`}>
-      <Header/>  
-    </div>
-  )
+    <>
+      <div id="page" className="w-screen h-screen select-none">
+        <Header lenisRef={lenisRef} />
+        <About />
+        <Skills />
+        <Projects />
+        <Closing />
+      </div>
+      <div id="portal-root" />
+    </>
+  );
 }
 
-export default App
+export default App;
