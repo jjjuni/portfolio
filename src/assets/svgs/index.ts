0c7bd04a3a5215be3d5fceae592f6fd0e0854bd4
jjjuni <0210leejun@gmail.com>
2026-03-09 17:32:21 +0900

    💄 style: 아이콘 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,3 +1,41 @@
 export { default as GithubIcon } from './github.svg?react';
 export { default as GithubHoverIcon } from './github_hover.svg?react';
-export { default as TestIcon } from './ishihara.svg?react';
+
+export { default as KIISIcon } from './kiis.svg?react';
+export { default as OpensourceIcon } from './opensource.svg?react';
+export { default as SKUnivIcon } from './skuniv.svg?react';
+export { default as UMCIcon } from './umc.svg?react';
+
+export { default as DayIcon } from './day_icon.svg?react';
+export { default as EmailIcon } from './email_icon.svg?react';
+export { default as PhoneIcon } from './phone_icon.svg?react';
+export { default as PinIcon } from './pin_icon.svg?react';
+
+export { default as AwsIcon } from './aws.svg?react';
+export { default as DockerIcon } from './docker.svg?react';
+export { default as FastapiIcon } from './fastapi.svg?react';
+export { default as FigmaIcon } from './figma.svg?react';
+export { default as FirebaseIcon } from './firebase.svg?react';
+export { default as ChromaIcon } from './chroma.svg?react';
+export { default as GitactionIcon } from './gitaction.svg?react';
+export { default as GsapIcon } from './gsap.svg?react';
+export { default as JavaIcon } from './java.svg?react';
+export { default as JsIcon } from './javascript.svg?react';
+export { default as TsIcon } from './typescript.svg?react';
+export { default as MongodbIcon } from './mongodb.svg?react';
+export { default as MysqlIcon } from './mysql.svg?react';
+export { default as NextjsIcon } from './nextjs.svg?react';
+export { default as NginxIcon } from './nginx.svg?react';
+export { default as PythonIcon } from './python.svg?react';
+export { default as ReactIcon } from './react.svg?react';
+export { default as SpringIcon } from './spring.svg?react';
+export { default as TailwindIcon } from './tailwind.svg?react';
+export { default as VercelIcon } from './vercel.svg?react';
+export { default as NpmIcon } from './npm.svg?react';
+export { default as OpenAIIcon } from './openai.svg?react';
+
+export { default as YoutubeIcon } from './youtube.svg?react';
+export { default as ExtensionIcon } from './extension.svg?react';
+
+export { default as ColbrushIcon } from './colbrush.svg?react';
+export { default as EmotreeIcon } from './emotree.svg?react';
