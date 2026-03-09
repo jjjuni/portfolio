929a58e5ef8f4ea7b6e9de5b862ac8162cbb3ef6
jjjuni <0210leejun@gmail.com>
2026-03-09 17:46:16 +0900

    ✨ feat: 헤더 토글 설정

---
+++ src/constants/Header.tsx
@@ -0,0 +1,21 @@
+export const ABOUT_APPEAR_RATIO = 0.1
+export const SKILLS_APPEAR_RATIO = 0.3
+export const PROJECTS_APPEAR_RATIO = 0.3
+
+export const HEADER_TOGGLE_LIST = [
+  {
+    title: 'ABOUT',
+    appearRatio: ABOUT_APPEAR_RATIO,
+    id: 'About-trigger'
+  },
+  {
+    title: 'SKILLS',
+    appearRatio: SKILLS_APPEAR_RATIO,
+    id: 'Skills-trigger'
+  },
+  {
+    title: 'PROJECTS',
+    appearRatio: PROJECTS_APPEAR_RATIO,
+    id: 'Projects-trigger'
+  }
+]
